6675c56c1f3df47dc64decf5b29f0902e1882924
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-01 17:41:11 +0300

    First Commit

--- src/ArrayRotate.java
+++ src/ArrayRotate.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 public class ArrayRotate {
 
     public static void rotate(int[] array,int k){
+        if (k>array.length) {
+            k=k%array.length;
+        }
+
         int[] newArray=new int[array.length];
         int j=0;
         for (int i = k+1; i < array.length; i++) {
@@ -12,12 +16,16 @@ public class ArrayRotate {
             newArray[j++]=array[i];
         }
 
-        System.out.println(Arrays.toString(newArray));
+        array = Arrays.copyOf(newArray, array.length);
+        System.out.println(Arrays.toString(array));
 
     }
+
+
 
     public static void main(String[] args) {
         int[] array= {1,2,3,4,5,6,7};
         rotate(array,3);
+        System.out.println(Arrays.toString(array));
     }
 }
