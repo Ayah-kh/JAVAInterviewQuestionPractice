373b5eccd8eb34f1d21d09956af2db2e82c384d4
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-01 18:44:42 +0300

    First Commit

--- src/ArrayRotate.java
+++ src/ArrayRotate.java
@@ -4,12 +4,14 @@ public class ArrayRotate {
 
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7};
-        ArrayRotate arrayRotate = new ArrayRotate();
-        arrayRotate.rotate(array, 3);
-        System.out.println(Arrays.toString(array));
+        int[] arrayV2 = {1, 2, 3, 4, 5, 6, 7};
+        rotate(array, 3);
+        rotateV2(arrayV2, 3);
+        System.out.println("First method: " + Arrays.toString(array));
+        System.out.println("Second method: " + Arrays.toString(arrayV2));
     }
 
-    public void rotate(int[] array, int k) {
+    public static void rotate(int[] array, int k) {
         if (k > array.length) {
             k = k % array.length;
         }
@@ -22,10 +24,36 @@ public class ArrayRotate {
         for (int i = 0; i <= k; i++) {
             newArray[j++] = array[i];
         }
-        System.arraycopy( newArray, 0, array, 0, array.length );
-
-//        array = Arrays.copyOf(newArray, array.length);
-        System.out.println(Arrays.toString(array));
+        System.arraycopy(newArray, 0, array, 0, array.length);
 
     }
+
+    public static void rotateV2(int[] array, int k) {
+
+        if (array == null || array.length == 0 || k < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+        if (k > array.length) {
+            k = k % array.length;
+        }
+
+        reverse(array, 0, k);
+        reverse(array, k + 1, array.length - 1);
+        reverse(array, 0, array.length - 1);
+    }
+
+    private static void reverse(int[] array, int start, int finish) {
+        if (array == null || array.length == 1)
+            return;
+        while (start < finish) {
+            int temp = array[start];
+            array[start] = array[finish];
+            array[finish] = temp;
+            start++;
+            finish--;
+
+        }
+    }
+
+
 }
