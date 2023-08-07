e320ad7e33888f4008e6712dc182301683d225e0
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-07 10:38:48 +0300

    First Commit

--- src/ReversePolishNotation/ReversePolishNotation.java
+++ src/ReversePolishNotation/ReversePolishNotation.java
@@ -1,7 +1,23 @@
 package ReversePolishNotation;
 
+import java.util.Stack;
+
 public class ReversePolishNotation {
-    public static int evaluate(String[] array){
+    public static int evaluate(String[] array) {
+        Stack<Integer> numberStack = new Stack<>();
+        Stack<String> operators = new Stack<>();
+        int result = 0;
+        for (String s : array) {
+            switch (s) {
+                case "*":
+                    int a = numberStack.pop();
+                    int b = numberStack.pop();
+                    result = a * b;
+                    numberStack.push(result);
+            }
+
+
+        }
 
 
     }
