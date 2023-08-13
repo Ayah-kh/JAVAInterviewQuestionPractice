8bba468b6e2b88b9409eaf14c77a77f5e107ab8e
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-13 14:27:29 +0300

    First Commit

--- src/IsomorphicStrings/IsomorphicStrings.java
+++ src/IsomorphicStrings/IsomorphicStrings.java
@@ -1,16 +1,36 @@
 package IsomorphicStrings;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class IsomorphicStrings {
     public static boolean isIsomorphic(String string1,String string2){
         if (string1.length()!=string2.length())
             return false;
-        string1.
+        Set<Character> uniqueCharacters1 = findUniqueCharacters(string1);
+        Set<Character> uniqueCharacters2 = findUniqueCharacters(string2);
+
+        if (uniqueCharacters1.size()!=uniqueCharacters2.size())
+            return false;
+
+
+
 
 
         return true;
     }
 
+    private static Set<Character> findUniqueCharacters(String str) {
+        Set<Character> uniqueChars = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            if (!uniqueChars.contains(c)) {
+                uniqueChars.add(c);
+            }
+        }
+        return uniqueChars;
+    }
+
     public static void main(String[] args) {
-        System.out.println("isIsomorphic(\"eff\",\"efff\") = " + isIsomorphic("eff", "efff"));
+        System.out.println("isIsomorphic(\"eff\",\"efg\") = " + isIsomorphic("eff", "efg"));
     }
 }
