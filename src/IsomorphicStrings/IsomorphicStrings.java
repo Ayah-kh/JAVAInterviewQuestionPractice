c469950f43307bcefdaa540ce6bda81945fa3892
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-13 14:12:34 +0300

    First Commit

--- src/IsomorphicStrings/IsomorphicStrings.java
+++ src/IsomorphicStrings/IsomorphicStrings.java
@@ -1,4 +1,16 @@
 package IsomorphicStrings;
 
 public class IsomorphicStrings {
+    public static boolean isIsomorphic(String string1,String string2){
+        if (string1.length()!=string2.length())
+            return false;
+        string1.
+
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("isIsomorphic(\"eff\",\"efff\") = " + isIsomorphic("eff", "efff"));
+    }
 }
